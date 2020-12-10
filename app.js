074bd3c4de510d234e91a6e0e5936e3aaad42d2f
dominicdaviescodes <codewithdom@gmail.com>
2020-12-10 20:19:37 +0000

    add countdown timer logic in js

---
+++ app.js
@@ -0,0 +1,14 @@
+const startTiming = 25;
+let time = startTiming * 60;
+
+const count = document.getElementById('countdownTimer');
+setInterval(updateTimer, 1000);
+function updateTimer() {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  // when less than 10 add 0 before number
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+  count.innerHTML = `${minutes}:${seconds}`;
+  time--;
+}
